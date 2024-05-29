82b4f32c76aeef0085c0e83a342b48bf4a241bc3
Derek Johnson <djohnson599@live.com>
2024-05-28 23:01:36 -0500

    implemented theme mode switching

--- board_game_tracker/lib/main.dart
+++ board_game_tracker/lib/main.dart
@@ -1,29 +1,12 @@
-import 'package:board_game_tracker/theme.dart';
+import 'package:board_game_tracker/theme/typography.dart';
+import 'package:board_game_tracker/theme/theme.dart';
 import 'package:board_game_tracker/widgets/navigation/mygames_page.dart';
 import 'package:board_game_tracker/widgets/navigation/searchgames_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Test Change',
-      theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: MaterialTheme.lightScheme().toColorScheme()),
-      darkTheme: ThemeData(
-          useMaterial3: true,
-          colorScheme: MaterialTheme.darkScheme().toColorScheme()),
-      home: const MyHomePage(title: 'Test Change'),
-    );
-  }
+  runApp(const MyHomePage(title: 'Board Game Tracker'));
 }
 
 class MyHomePage extends StatefulWidget {
@@ -37,6 +20,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Widget _page = const MyGamesPage();
+  MaterialScheme _currentTheme = MaterialTheme.darkScheme();
+  Icon _themeIcon = const Icon(Icons.mode_night);
 
   // Snake bar variables
   ShapeBorder? bottomBarShape = const RoundedRectangleBorder(
@@ -48,75 +33,91 @@ class _MyHomePageState extends State<MyHomePage> {
   int _selectedItemPosition = 2;
   SnakeShape snakeShape = SnakeShape.circle;
 
-  bool showSelectedLabels = false;
-  bool showUnselectedLabels = false;
-
-  Color selectedColor = Colors.black;
-  Color unselectedColor = Colors.blueGrey;
-
-  Gradient selectedGradient =
-      const LinearGradient(colors: [Colors.red, Colors.amber]);
-  Gradient unselectedGradient =
-      const LinearGradient(colors: [Colors.red, Colors.blueGrey]);
-
-  Color? containerColor;
-  List<Color> containerColors = [
-    const Color(0xFFFDE1D7),
-    const Color(0xFFE4EDF5),
-    const Color(0xFFE7EEED),
-    const Color(0xFFF4E4CE),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: false,
-      resizeToAvoidBottomInset: true,
-      extendBody: true,
-      appBar: AppBar(
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Board Game Tracker',
+      theme: ThemeData(
+        colorScheme: _currentTheme.toColorScheme(),
+        textTheme: textTheme,
+        appBarTheme: AppBarTheme(
+            color: _currentTheme.primaryContainer,
+            foregroundColor: _currentTheme.onPrimaryContainer),
       ),
-      body: Center(child: _page),
-      bottomNavigationBar: SnakeNavigationBar.color(
-        behaviour: snakeBarStyle,
-        snakeShape: snakeShape,
-        shape: bottomBarShape,
-        padding: padding,
+      home: Scaffold(
+        extendBodyBehindAppBar: false,
+        resizeToAvoidBottomInset: true,
+        extendBody: true,
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: [
+            IconButton(
+                onPressed: () => setState(() {
+                      if (_currentTheme == MaterialTheme.darkScheme()) {
+                        _currentTheme = MaterialTheme.lightScheme();
+                        _themeIcon = const Icon(Icons.light_mode);
+                      } else {
+                        _currentTheme = MaterialTheme.darkScheme();
+                        _themeIcon = const Icon(Icons.mode_night);
+                      }
+                    }),
+                icon: _themeIcon)
+          ],
+        ),
+        body: Center(child: _page),
+        bottomNavigationBar: SnakeNavigationBar.color(
+          behaviour: snakeBarStyle,
+          snakeShape: snakeShape,
+          shape: bottomBarShape,
+          padding: padding,
+          backgroundColor: _currentTheme.secondaryContainer,
 
-        ///configuration for SnakeNavigationBar.color
-        snakeViewColor: selectedColor,
-        selectedItemColor:
-            snakeShape == SnakeShape.indicator ? selectedColor : null,
-        unselectedItemColor: unselectedColor,
+          ///configuration for SnakeNavigationBar.color
+          snakeViewColor: _currentTheme.inversePrimary,
+          selectedItemColor: snakeShape == SnakeShape.indicator
+              ? _currentTheme.inversePrimary
+              : null,
+          unselectedItemColor: _currentTheme.inverseOnSurface,
 
-        ///configuration for SnakeNavigationBar.gradient
-        // snakeViewGradient: selectedGradient,
-        // selectedItemGradient: snakeShape == SnakeShape.indicator ? selectedGradient : null,
-        // unselectedItemGradient: unselectedGradient,
+          showUnselectedLabels: false,
+          showSelectedLabels: false,
 
-        showUnselectedLabels: showUnselectedLabels,
-        showSelectedLabels: showSelectedLabels,
-
-        currentIndex: _selectedItemPosition,
-        onTap: (index) => setState(() {
-          if (index == 3) {
-            _page = const SearchGamesPage();
-          } else {
-            _page = const MyGamesPage();
-          }
-          _selectedItemPosition = index;
-        }),
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notifications), label: 'tickets'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.access_alarm), label: 'calendar'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'settings')
-        ],
-        selectedLabelStyle: const TextStyle(fontSize: 14),
-        unselectedLabelStyle: const TextStyle(fontSize: 10),
+          currentIndex: _selectedItemPosition,
+          onTap: (index) => setState(() {
+            if (index == 3) {
+              _page = const SearchGamesPage();
+            } else {
+              _page = const MyGamesPage();
+            }
+            _selectedItemPosition = index;
+          }),
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.notifications,
+                  color: _currentTheme.onSecondaryContainer,
+                ),
+                label: 'tickets'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.access_alarm,
+                    color: _currentTheme.onSecondaryContainer),
+                label: 'calendar'),
+            BottomNavigationBarItem(
+                icon:
+                    Icon(Icons.home, color: _currentTheme.onSecondaryContainer),
+                label: 'home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search,
+                    color: _currentTheme.onSecondaryContainer),
+                label: 'search'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings,
+                    color: _currentTheme.onSecondaryContainer),
+                label: 'settings')
+          ],
+          selectedLabelStyle: const TextStyle(fontSize: 14),
+          unselectedLabelStyle: const TextStyle(fontSize: 10),
+        ),
       ),
     );
   }
